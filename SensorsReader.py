4545a60a9f20c796fd337d7dacc9c82b63f4e159
Huzaifa Irfan <huzaifairfan2001@gmail.com>
2023-05-02 19:36:44 +0500

    save readings to csv

--- SensorsReader.py
+++ SensorsReader.py
@@ -7,27 +7,82 @@ class SensorsReader:
 
     def convertRawReading(self):
 
-        self.Readings["temp0"]=self.RawReadings["temp0"]
-        self.Readings["temp1"]=self.RawReadings["temp1"]
-        self.Readings["temp2"]=self.RawReadings["temp2"]
-        self.Readings["temp3"]=self.RawReadings["temp3"]
+        self.CalibratedReadings["temp0"]=self.RawReadings["temp0"]
+        self.CalibratedReadings["temp1"]=self.RawReadings["temp1"]
+        self.CalibratedReadings["temp2"]=self.RawReadings["temp2"]
+        self.CalibratedReadings["temp3"]=self.RawReadings["temp3"]
 
-        self.Readings["temp4"]=self.RawReadings["temp4"]
-        self.Readings["temp5"]=self.RawReadings["temp5"]
-        self.Readings["temp6"]=self.RawReadings["temp6"]
-        self.Readings["temp7"]=self.RawReadings["temp7"]
+        self.CalibratedReadings["temp4"]=self.RawReadings["temp4"]
+        self.CalibratedReadings["temp5"]=self.RawReadings["temp5"]
+        self.CalibratedReadings["temp6"]=self.RawReadings["temp6"]
+        self.CalibratedReadings["temp7"]=self.RawReadings["temp7"]
 
-        self.Readings["pressure0"]=self.RawReadings["analog0"]/1024*5
-        self.Readings["pressure1"]=self.RawReadings["analog1"]/1024*5
-        self.Readings["pressure2"]=self.RawReadings["analog2"]/1024*5
-        self.Readings["pressure3"]=self.RawReadings["analog3"]/1024*5
+        self.CalibratedReadings["pressure0"]=((self.RawReadings["analog0"]/1024*5)-0.5)/4*1200
+        self.CalibratedReadings["pressure1"]=((self.RawReadings["analog1"]/1024*5)-0.5)/4*1200
+        self.CalibratedReadings["pressure2"]=((self.RawReadings["analog2"]/1024*5)-0.5)/4*1200
+        self.CalibratedReadings["pressure3"]=((self.RawReadings["analog3"]/1024*5)-0.5)/4*1200
 
-        self.Readings["flowRate0"]=self.RawReadings["interruptRate0"]*60/5.5
-        self.Readings["flowRate1"]=self.RawReadings["interruptRate1"]*60/5.5
-        
+        self.CalibratedReadings["flowRate0"]=self.RawReadings["interruptRate0"]*60/5.5
+        self.CalibratedReadings["flowRate1"]=self.RawReadings["interruptRate1"]*60/5.5
+
+
+        print('----- Temperature Sensors (Centigrade) -----')
+        print(f' Tube In: {self.CalibratedReadings["temp4"]}')
+        print(f' Tube Out: {self.CalibratedReadings["temp6"]}')
+
+        print(f' Shell In: {self.CalibratedReadings["temp0"]}')
+        print(f' Shell 1: {self.CalibratedReadings["temp3"]}')
+        print(f' Shell 2: {self.CalibratedReadings["temp7"]}')
+        print(f' Shell 3: {self.CalibratedReadings["temp1"]}')
+        print(f' Shell 4: {self.CalibratedReadings["temp5"]}')
+        print(f' Shell Out: {self.CalibratedReadings["temp2"]}')
+
+
+    def LabelReading(self):
+
+        self.LabelledReadings["tempTubeIn"]=self.CalibratedReadings["temp4"]
+        self.LabelledReadings["tempTubeOut"]=self.CalibratedReadings["temp6"]
+
+        self.LabelledReadings["tempShellIn"]=self.CalibratedReadings["temp0"]
+        self.LabelledReadings["tempShell1"]=self.CalibratedReadings["temp3"]
+        self.LabelledReadings["tempShell2"]=self.CalibratedReadings["temp7"]
+        self.LabelledReadings["tempShell3"]=self.CalibratedReadings["temp1"]
+        self.LabelledReadings["tempShell4"]=self.CalibratedReadings["temp5"]
+        self.LabelledReadings["tempShellOut"]=self.CalibratedReadings["temp2"]
+
+        self.LabelledReadings["pressureTubeIn"]=self.CalibratedReadings["pressure0"]
+        self.LabelledReadings["pressureTubeOut"]=self.CalibratedReadings["pressure1"]
+        self.LabelledReadings["pressureShellIn"]=self.CalibratedReadings["pressure2"]
+        self.LabelledReadings["pressureShellOut"]=self.CalibratedReadings["pressure3"]
+
+        self.LabelledReadings["flowRateTube"]=self.CalibratedReadings["flowRate0"]
+        self.LabelledReadings["flowRateShell"]=self.CalibratedReadings["flowRate1"]       
 
     def __init__(self, comport) -> None:
+
+
+        self.LabelledReadings = {
+            "tempTubeIn": 0,
+            "tempTubeOut": 0,
+
+            "tempShellIn": 0,
+            "tempShell1": 0,
+            "tempShell2": 0,
+            "tempShell3": 0,
+            "tempShell4": 0,
+            "tempShellOut": 0,
+
+            "pressureTubeIn": 0,
+            "pressureTubeOut": 0,
+            "pressureShellIn": 0,
+            "pressureShellOut": 0,
+
+            "flowRateTube": 0,
+            "flowRateShell": 0
+        }   
+
         self.RawReadings = {
+            
             "temp0": 0,
             "temp1": 0,
             "temp2": 0,
@@ -47,7 +102,7 @@ class SensorsReader:
             "interruptRate1": 0
         }
 
-        self.Readings = {
+        self.CalibratedReadings = {
             "temp0": 0,
             "temp1": 0,
             "temp2": 0,
@@ -65,7 +120,8 @@ class SensorsReader:
 
             "flowRate0": 0,
             "flowRate1": 0
-        }     
+        }    
+
 
 
         self.comport = comport
@@ -105,15 +161,48 @@ class SensorsReader:
         print('----- Sensor Readings -----')
 
         print('----- Temperature Sensors (Centigrade) -----')
-        print(f'{self.Readings["temp0"]}, {self.Readings["temp1"]}, {self.Readings["temp2"]}, {self.Readings["temp3"]}, {self.Readings["temp4"]}, {self.Readings["temp5"]}, {self.Readings["temp6"]}, {self.Readings["temp7"]}')
+        print(f'{self.CalibratedReadings["temp0"]}, {self.CalibratedReadings["temp1"]}, {self.CalibratedReadings["temp2"]}, {self.CalibratedReadings["temp3"]}, {self.CalibratedReadings["temp4"]}, {self.CalibratedReadings["temp5"]}, {self.CalibratedReadings["temp6"]}, {self.CalibratedReadings["temp7"]}')
 
-        print('----- Pressure Sensors (KPIa) -----')
+        print('----- Pressure Sensors (KPa) -----')
         print(
-            f'{self.Readings["pressure0"]}, {self.Readings["pressure1"]}, {self.Readings["pressure2"]}, {self.Readings["pressure3"]}')
+            f'{self.CalibratedReadings["pressure0"]}, {self.CalibratedReadings["pressure1"]}, {self.CalibratedReadings["pressure2"]}, {self.CalibratedReadings["pressure3"]}')
 
         print('----- Flow Sensors (LPM) -----')
         print(
-            f'{self.Readings["flowRate0"]}, {self.Readings["flowRate1"]}')
+            f'{self.CalibratedReadings["flowRate0"]}, {self.CalibratedReadings["flowRate1"]}')
+
+
+
+ 
+
+
+    def printLabeledReadings(self):
+
+        print('----- Sensor Readings -----')
+
+        print('----- Temperature Sensors (Centigrade) -----')
+        print(f' Tube In: {self.LabelledReadings["tempTubeIn"]}')
+        print(f' Tube Out: {self.LabelledReadings["tempTubeOut"]}')
+
+        print(f' Shell In: {self.LabelledReadings["tempShellIn"]}')
+        print(f' Shell 1: {self.LabelledReadings["tempShell1"]}')
+        print(f' Shell 2: {self.LabelledReadings["tempShell2"]}')
+        print(f' Shell 3: {self.LabelledReadings["tempShell3"]}')
+        print(f' Shell 4: {self.LabelledReadings["tempShell4"]}')
+        print(f' Shell Out: {self.LabelledReadings["tempShellOut"]}')
+
+        print('----- Pressure Sensors (KPa) -----')
+        print(f' Tube In: {self.LabelledReadings["pressureTubeIn"]}')
+        print(f' Tube Out: {self.LabelledReadings["pressureTubeOut"]}')
+
+        print(f' Shell In: {self.LabelledReadings["pressureShellIn"]}')
+        print(f' Shell Out: {self.LabelledReadings["pressureShellOut"]}')
+
+        print('----- Flow Sensors (LPM) -----')
+        print(f'Tube Out: {self.LabelledReadings["flowRateTube"]}')
+        print(f'Shell Out: {self.LabelledReadings["flowRateShell"]}')
+        
+    
         
     def read(self):
 
@@ -200,4 +289,4 @@ class SensorsReader:
         return self.RawReadings
     
     def getReadings(self):
-        return self.Readings
+        return self.CalibratedReadings
--- main.py
+++ main.py
@@ -1,19 +1,48 @@
 
 from SensorsReader import SensorsReader
+import csv
+from datetime import datetime
 
 
+fieldnames = [
+    "timestamp",
 
+    "tempTubeIn",
+    "tempTubeOut",
+
+    "tempShellIn",
+    "tempShell1",
+    "tempShell2",
+    "tempShell3",
+    "tempShell4",
+    "tempShellOut",
+
+    "pressureTubeIn",
+    "pressureTubeOut",
+    "pressureShellIn",
+    "pressureShellOut",
+
+    "flowRateTube",
+    "flowRateShell"
+]
 
 if __name__ == '__main__':
-    sensorsReader=SensorsReader('COM14')
-    i=0
+    sensorsReader = SensorsReader('COM3')
+    i = 0
 
-    while(1):
-        if(sensorsReader.read()):
+
+    while (1):
+        if (sensorsReader.read()):
             sensorsReader.convertRawReading()
-            sensorsReader.printReadings()
+            sensorsReader.LabelReading()
+            sensorsReader.printLabeledReadings()
             print(f"----------------{i}----------------")
-            i=i+1
-            # print(sensorsReader.getRawReadings()["temp0"])
-            # print(sensorsReader.getRawReadings())
-        
+            i = i+1
+
+            with open('readings.csv', 'a', encoding='UTF8', newline='') as f:
+                writer = csv.DictWriter(f, fieldnames=fieldnames)
+                if f.tell() == 0:
+                    writer.writeheader()
+                sensorsReader.LabelledReadings["timestamp"] = datetime.now()
+                writer.writerow(sensorsReader.LabelledReadings)
+
